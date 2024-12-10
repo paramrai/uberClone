bfa5f58df06b03db110b989ddf7728baf111d8c6
Param-rai <paramrai0202@gmail.com>
2024-12-10 21:18:42 +0530

    start page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Start from "./pages/Start";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
 
 const App = () => {
-  return <div className="text-3xl underline text-red-800">hello world</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
