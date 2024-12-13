b934cb1afbf6de7e4bfe83bcecefba0be19cce3b
Param-rai <paramrai0202@gmail.com>
2024-12-13 21:46:02 +0530

    UserLogout done

---
+++ frontend/src/pages/UserLogout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserLogout = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  axios
+    .get(`${import.meta.env.VITE_BASE_URL}/users/logout`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      }
+    });
+
+  return;
+};
+
+export default UserLogout;
