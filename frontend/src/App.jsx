863fdb9ab817d06dc4e0b12602d035de0eec20c1
Param-rai <paramrai0202@gmail.com>
2024-12-13 22:40:06 +0530

    UserProtectWrapper done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignup";
 import CaptainLogin from "./pages/CaptainLogin";
 import CaptainSignup from "./pages/CaptainSignup";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
+import UserLogout from "./pages/UserLogout";
 
 const App = () => {
   return (
@@ -18,7 +20,23 @@ const App = () => {
         <Route path="/captain-login" element={<CaptainLogin />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
 
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          }
+        />
+
+        <Route
+          path="user/logout"
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }
+        />
       </Routes>
     </>
   );
---
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
+
+const UserProtectWrapper = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const { user, setUser } = useContext(UserDataContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: { authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setUser(response.data.user);
+        }
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        localStorage.removeItem("token");
+        navigate("/login");
+        setIsLoading(false);
+      });
+  }, [token]);
+
+  if (isLoading) {
+    return <div>Loading...</div>; // Show loading indicator here while waiting for user data.
+  }
+
+  return <div>{children}</div>;
+};
+
+export default UserProtectWrapper;
